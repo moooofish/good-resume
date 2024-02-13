7aa4ff69b5da8965d60fea64806a8e5e0f503599
TD-gaowei <wei.gao@china.talkdesk.com>
2024-02-13 18:06:34 +0800

    init project

---
+++ docs/.island/config.ts
@@ -0,0 +1,66 @@
+import { defineConfig } from "islandjs";
+
+export default defineConfig({
+  title: "如何制作好简历",
+  themeConfig: {
+    sidebar: {
+      "/": [
+        {
+          text: "简历构成",
+          items: [
+            {
+              text: "基本信息",
+              link: "/article/base-info",
+            },
+            {
+              text: "专业技能",
+              link: "/article/skills",
+            },
+            {
+              text: "工作经历",
+              link: "/article/works",
+            },
+            {
+              text: "项目经历",
+              link: "/article/project",
+            },
+            {
+              text: "教育经历",
+              link: "/article/edc",
+            },
+            {
+              text: "个人成就",
+              link: "/article/achievements",
+            },
+            {
+              text: "自我评价",
+              link: "/article/self-evaluation",
+            },
+          ],
+        },
+        {
+          text: "在线简历",
+          items: [
+            {
+              text: "VuePress 搭建在线简历",
+              link: "/online-resume/vuepress",
+            },
+            {
+              text: "Island.js 搭建在线简历",
+              link: "/online-resume/islandjs",
+            },
+          ],
+        },
+        {
+          text: "其他",
+          items: [
+            {
+              text: "总结",
+              link: "/others/summary",
+            },
+          ],
+        },
+      ],
+    },
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+pageType: home
+
+hero:
+  name: 如何制作好简历
+  # text:
+  tagline: 好简历可以让 HR 快速知道你的求职目的、你与应聘岗位的匹配程度，从而决定是否向你发起面试邀请
+  # image:
+  #   src: /logo.png
+  #   alt: Note
+  actions:
+    - theme: brand
+      text: 点击查看
+      link: /article/base-info
+    - theme: alt
+      text: GitHub
+      link: https://github.com/unikww
+features:
+  - title: 适用 IT 打工人
+    # details: Feature 1 的详细内容
+    icon: 🪐
+  - title: 内容简单、易上手
+    # details: Feature 2 的详细内容
+    icon: 🧑🏻‍💻
+  - title: 开源免费
+    # details: Feature 3 的详细内容
+    icon: 🏃‍♂️
+---
